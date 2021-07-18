8e584e72971f2bfd70ef5399466a5f44c88c649d
vbjc5275 <vbjc5275@gmail.com>
2021-07-18 22:08:27 +0800

    create

--- test/test.R
+++ test/test.R
@@ -5,11 +5,9 @@ library(bnTool)
 library(devtools)
 install_github('vbjc5275/bnTool')
 
-install.packages("vbjc5275/bnTool")
 
 data(coronary)
 bn_model = tabu(coronary)
-bn_model
 view = create_from_to_viewer(bn_model)
 
 
